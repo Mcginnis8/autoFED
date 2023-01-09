2b3660c25cb5cad5e6ffd757dbe55bc27f2e2831
Cole McGinnis <camvols19@gmail.com>
2023-01-09 14:47:57 -0500

    fixed env variables

--- modules/firebase.js
+++ modules/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDMukKGDlQeJspjbdDvmZmXYGQivAckNIg",
+    authDomain: "automatedfed.firebaseapp.com",
+    projectId: "automatedfed",
+    storageBucket: "automatedfed.appspot.com",
+    messagingSenderId: "570540351539",
+    appId: "570540351539:web:588499ed3c69d3c8ce4346",
+    measurementId: ":G-M2TKXESLXW"
 };
 
 
